812809e872c2a63a59de02af52b8aac2fb9f5ed0
Pedro Ítalo <pedroitalomatos@gmail.com>
2022-08-27 19:18:32 -0300

    Refatoração da classe palavra.

--- Palavra.js
+++ Palavra.js
@@ -1,10 +1,38 @@
+const vetorPalavras = [
+    {
+        palavra: 'boi',
+        qntdLetras: 3,
+        qntdMaxDicas: 1,
+        dicas: ['é um animal']
+    },
+    {
+        palavra: 'panela',
+        qntdLetras: 6,
+        qntdMaxDicas: 2,
+        dicas: ['é um substantivo', 'lugar habitual é na cozinha']
+    },
+    {
+        palavra: 'cofre',
+        qntdLetras: 5,
+        qntdMaxDicas: 2,
+        dicas: ['é um substantivo', 'usado para guardar coisas valiosas']
+    },
+    {
+        palavra: 'babana',
+        qntdLetras: 5,
+        qntdMaxDicas: 2,
+        dicas: ['é um substantivo', 'é uma fruta', 'tem três vogais iguais']
+    }
+]
+
 module.exports = class Palavra {
-    //propriedades e funções da classe aqui
     constructor() {
-        //this.palavra = ['t', 'e', 's', 't', 'e'];
-        this.palavra = 'teste';
-        this.qntdLetras = 5;
-        this.qntdMaxDicas = 3;
+        const index = this.between(0, vetorPalavras.length - 1);
+        // simulando pegar do banco de dados
+        this.palavra = vetorPalavras[index].palavra;
+        this.qntdLetras = vetorPalavras[index].qntdLetras;
+        this.qntdMaxDicas = vetorPalavras[index].qntdMaxDicas;
+        this.dicas = vetorPalavras[index].dicas;
     }
 
     RetornaPalavra (){
@@ -16,10 +44,15 @@ module.exports = class Palavra {
     }
 
     RetornaDicas(idDica){
-        const vetorDicas = ['começa com t', 'tem dois pares de letras iguais', 'é um subistantivo'];
         if(idDica > this.qntdMaxDicas){
             return 'não tem mais dicas';
         }
-        return vetorDicas[idDica - 1];
+        return this.dicas[idDica - 1];
+    }
+
+    between(min, max) {  
+        return Math.floor(
+          Math.random() * (max - min) + min
+        )
     }
 }
